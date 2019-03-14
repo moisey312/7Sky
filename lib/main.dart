3f33f62de0c4b4473bc89a2d3b6923f7b0325020
daniilshilintsev <1206dan>
2019-03-14 19:55:57 +0300

    testik

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,9 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
+
+
+
       home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
@@ -95,6 +98,7 @@ class _MyHomePageState extends State<MyHomePage> {
               'You have pushed the button this many times:',
             ),
             Text(
+
               '$_counter',
               style: Theme.of(context).textTheme.display1,
             ),
@@ -105,7 +109,7 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
