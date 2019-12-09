5591ce48ade0a42091aa2e2e7290e01d05e8c4f5
moisey312 <moisey312@gmail.com>
2019-12-09 23:52:10 +0300

    незначительные изменения

--- lib/pages/sign_up.dart
+++ lib/pages/sign_up.dart
@@ -108,28 +108,6 @@ class _SignUpState extends State<SignUp> {
     );
   }
 
-//  void _showVerifyEmailSentDialog() {
-//    showDialog(
-//      context: context,
-//      builder: (BuildContext context) {
-//        // return object of type Dialog
-//        return AlertDialog(
-//          title: new Text("Verify your account"),
-//          content:
-//              new Text("Link to verify account has been sent to your email"),
-//          actions: <Widget>[
-//            new FlatButton(
-//              child: new Text("Dismiss"),
-//              onPressed: () {
-//                toggleFormMode();
-//                Navigator.of(context).pop();
-//              },
-//            ),
-//          ],
-//        );
-//      },
-//    );
-//  }
   //виджет самого контейнера, который отображается
   Widget _showForm() {
     return new Container(
