9a251380bfa3c2c2325529512397555d60dcf6e2
moisey <moisey312@gmail.com>
2020-01-07 14:59:57 +0300

    start 0

--- lib/models/storage.dart
+++ lib/models/storage.dart
@@ -1,5 +1,5 @@
 import 'package:firebase_storage/firebase_storage.dart';
 class Storage{
-  final StorageReference storageRef =
-  FirebaseStorage.instance.ref().child(fileName);
+  //final StorageReference storageRef =
+  //FirebaseStorage.instance.ref().child(fileName);
 }
